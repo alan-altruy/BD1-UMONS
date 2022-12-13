b3a38a58b9dc07110c008a5458fe43270bee98b1
alan-altruy <200161@umons.ac.be>
2022-12-13 23:41:17 +0100

    Update validation de requête sql dans le menu principal (encore)

--- src/spjrud.py
+++ src/spjrud.py
@@ -339,7 +339,6 @@ class SPJRUD:
     def is_sql_query(self, query: str):
         try:
             self.cursor.execute(query)
-            self.cursor.execute("commit")
             data = self.cursor.fetchall()
             if len(data) == 0:
                 return False
